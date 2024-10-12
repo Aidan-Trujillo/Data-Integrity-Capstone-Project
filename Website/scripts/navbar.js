90e7376ff22bac0d39c9162ba2cea9411ba0408e
Aidan-Trujillo <ajt494@nau.edu>
2024-10-11 17:51:16 -0700

    Adding a new navbar

---
+++ Website/scripts/navbar.js
@@ -0,0 +1,16 @@
+
+function loadNavBar() {
+        
+        document.getElementById('navigationbar').innerHTML = 
+            `<!--Navigation bar on the top of the website-->
+            <nav class="navigationbar">
+
+                <!--Links for each section-->
+                <div class="links"><img src="./assets/logo.png"; height="100px" width="auto"></div>
+                <div class="links"><a href="index.html">Home</a></div>
+                <div class="links"><a href="#">Project</a></div>
+                <div class="links"><a href="team.html">Team</a></div>
+                <div class="links"><a href="about.html">About</a></div>
+                <div class="links"><a href="#">Documentation</a></div>
+            </nav>`;
+}
